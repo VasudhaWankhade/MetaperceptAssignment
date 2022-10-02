ca1d9fd1d2dee0e971aac2b44d42ae8c6aa74e10
VasudhaWankhade <vashudhawankhade@gmail.com>
2022-10-02 10:00:53 +0530

    print() dataTypes operators

---
+++ 1.py
@@ -0,0 +1,44 @@
+a = 10
+print(a)
+
+print(type(a)) #int
+
+b = 20.00
+print(type(b)) #float --> decimal points
+
+c = "Vasudha"
+print(type(c)) # string
+
+d = 'A'
+print(type(d))  # string
+
+e = True
+print(type(e))
+
+# DataTypes ==> int     float   boolean     string
+
+# Variables ==> identifiers 
+# should start with alphabets or underscore
+# should not include any special characters except _
+# should not include blank space
+# can contain numbers
+
+# Comparision operators
+print(10 < 11)
+print(100 > 12)
+print(10 <= 10)
+print(100 >= 99)
+print(10 != 10)
+print(100 == 100)
+
+# Logical operators 
+# AND       OR      NOT
+# and --> if both true ==> true else false
+# or  --> if both false ==> false else true
+# not --> if true ==> false if false ==> true
+
+print(100 ==100 and 200 > 100)
+print(100 > 100 or 10 == 10)
+print(not True)
+
+
